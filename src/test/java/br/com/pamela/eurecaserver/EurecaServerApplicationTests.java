a77b78a8939dc2caeaba22d23971105d77350658
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-09-01 16:39:08 -0300

    Eureka Server criado e conectado com as apis payroll e user

---
+++ src/test/java/br/com/pamela/eurecaserver/EurecaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pamela.eurecaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurecaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
